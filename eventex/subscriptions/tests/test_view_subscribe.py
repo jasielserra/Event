07a6f71b393442147577f5a7cb6cb9044e0998fc
Jasiel Serra <jasiel_serra@hotmail.com>
2016-06-15 00:43:14 -0300

    Persiste a inscricao

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,6 +1,8 @@
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
+
 
 class SubscribeGet(TestCase):
     def setUp(self):
@@ -57,6 +59,9 @@ class SubscribePostInvalid(TestCase):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
 
+    def test_dont_save_subscription(self):
+        self.assertFalse(Subscription.objects.exists())
+
 class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name='Jasiel Serra', cpf='12345678901',
@@ -70,6 +75,9 @@ class SubscribePostValid(TestCase):
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
+
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
 
 class SubscribeSuccessManager(TestCase):
     def test_message(self):
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -27,6 +28,7 @@ def create(request):
                form.cleaned_data)
 
     # Success feedback
+    Subscription.objects.create(**form.cleaned_data)
     messages.success(request,'Inscrição realizada com sucesso!')
 
     return HttpResponseRedirect('/inscricao/')
