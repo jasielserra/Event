ffae77a6fcd5ad9d1b4ec4a0f1ab85623472731a
Jasiel Serra <jasiel_serra@hotmail.com>
2016-06-27 21:58:18 -0300

    Email ou Telefone opcionais.

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -82,3 +82,11 @@ class SubscriptionsNewPostValid(TestCase):
         self.assertTrue(Subscription.objects.exists())
 
 
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_erros(self):
+        invalid_data = dict(name='Jasiel Serra', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+
+        self.assertContains(response,'<ul class="errorlist nonfield">')
+
+
